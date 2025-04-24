6bbf44d0ef81e7efa22db1ed02c8db13084414be
John Krbaio <krbaio3@gmail.com>
2025-04-24 10:40:46 +0200

    feat(API): bendice .env

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
-import 'dotenv/config';
 import express from "express";
 import mongoose from "mongoose";
 import cors            from "cors";
 import versiculoRoutes from './routes/versiculos';
-
+if (process.env.NODE_ENV !== 'production') {
+    const dotenv = import 'dotenv/config';
+}
 const app = express();
 app.use(cors());
 app.use(express.json());
--- src/utils/mongo.config.js
+++ src/utils/mongo.config.js
@@ -1,4 +1,6 @@
-import 'dotenv/config';
+if (process.env.NODE_ENV !== 'production') {
+    import 'dotenv/config';
+}
 
 export const mongoConfig = () => {
     const {
